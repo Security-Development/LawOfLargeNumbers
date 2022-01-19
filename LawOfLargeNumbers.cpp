1cc1771804d61a9cd776edcdb68ba9a6c88d82db
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-01-19 22:55:25 +0900

    Update LawOfLargeNumbers.cpp

--- LawOfLargeNumbers.cpp
+++ LawOfLargeNumbers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int* sort(int* arr, int size) 
 {
 
-    for(int i = 0; i <= size - 1; i++)
+    for(int i = 0; i < size; i++)
     {
         for( int j = i + 1; j <= size; j++)
         {
